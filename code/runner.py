90f8c9b5eda69d82b9f84150bacb0592666fd24c
mikedelong <mikedelong@outlook.com>
2018-04-27 13:58:55 -0400

    feature: first working version; this running will run every Python script in the folder except itself.

---
+++ code/runner.py
@@ -0,0 +1,32 @@
+import logging
+import time
+from os import listdir
+from os.path import basename
+
+start_time = time.time()
+
+suffix = '.py'
+if __name__ == '__main__':
+    formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+    logger = logging.getLogger('main')
+    logger.setLevel(logging.DEBUG)
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    console_handler.setLevel(logging.DEBUG)
+    logger.debug('started')
+
+    base_file_name = basename(__file__)
+
+    for script_file in listdir('.'):
+        if script_file.endswith(suffix) and script_file != base_file_name:
+            logger.debug('we are running %s' % script_file)
+            exec(open(script_file).read(), globals())
+
+    logger.debug('done')
+    finish_time = time.time()
+    elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
+    elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
+    logger.info('Time: {:0>2}:{:0>2}:{:05.2f}'.format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
+    console_handler.close()
+    logger.removeHandler(console_handler)
